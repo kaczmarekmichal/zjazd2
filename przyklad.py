8a90eefd4f1a807cf9a58047034c0093054c0dbf
kaczmarekmichal <kaczmarekmichal@users.noreply.github.com>
2019-01-20 13:15:41 +0100

    testy f7

--- przyklad.py
+++ przyklad.py
@@ -16,3 +16,56 @@ def f2(x):
         return x[0]
     else:
         return "BUUUM"
+
+def f3(x):
+    if x<=3:
+        if x==1:
+            return "jeden"
+        if x==2:
+            return "dwa"
+        if x==3:
+            return "trzy"
+    else:
+        return "other"
+
+def f4(x ,y=""):
+    if len(y)==0:
+        return x+ " ma kota"
+    else:
+        return x+ " ma kota i "+y
+
+def f5 (x, y=0):
+    tablica = []
+
+    if y==0:
+
+        if x==0:
+            return []
+        if x==1:
+            return [x-1]
+        if x>1:
+            for a in range (x):
+                tablica.append(a)
+            return tablica
+    if y>1:
+        for a in range (x/y+1):
+            tablica.append(a*y)
+        return tablica
+
+def f6(x):
+    return "*"*x
+
+def f7(x):
+    if isinstance(x, str):
+        if x.isspace():
+            return "zdanie"
+        else:
+            return "slowo"
+    if isinstance(x, int):
+        if 0<=x<10:
+            return "cyfra"
+        else:
+            if x<0:
+                return "liczba ze znakiem"
+            else:
+                return "liczba"
--- test_przyklad.py
+++ test_przyklad.py
@@ -2,6 +2,8 @@
 
 import unittest
 import przyklad
+import random
+
 
 class klasa_testowa (unittest.TestCase):
             def test_test(self):
@@ -38,6 +40,98 @@ class klasa_testowa (unittest.TestCase):
             def test_f2_3(self):
                 w=przyklad.f2([])
                 self.assertEqual(w,"BUUUM")
+
+            def test_f3_1(self):
+                w=przyklad.f3(1)
+                self.assertEqual(w,"jeden")
+
+            def test_f3_2(self):
+                w=przyklad.f3(2)
+                self.assertEqual(w,"dwa")
+
+            def test_f3_3(self):
+                w=przyklad.f3(3)
+                self.assertEqual(w,"trzy")
+
+            def test_f3_4(self):
+                w=przyklad.f3(random.choice(range(4,1000)))
+                self.assertEqual(w,"other")
+
+            def test_4_1(self):
+                w=przyklad.f4("ala")
+                self.assertEqual(w,"ala ma kota")
+
+            def test_4_2(self):
+                w=przyklad.f4("kot")
+                self.assertEqual(w,"kot ma kota")
+
+            def test_4_3(self):
+                w=przyklad.f4("kot","psa")
+                self.assertEqual(w,"kot ma kota i psa")
+
+            def test_4_4(self):
+                w=przyklad.f4("kot","mysz")
+                self.assertEqual(w,"kot ma kota i mysz")
+
+            def test_f5_1(self):
+                w=przyklad.f5(0)
+                self.assertEqual(w,[])
+
+            def test_f5_2(self):
+                w=przyklad.f5(1)
+                self.assertEqual(w,[0])
+
+            def test_f5_3(self):
+                w=przyklad.f5(2)
+                self.assertEqual(w,[0,1])
+
+            def test_f5_4(self):
+                w=przyklad.f5(7)
+                self.assertEqual(w,[0,1,2,3,4,5,6])
+
+            def test_f5_5(self):
+                w=przyklad.f5(7,2)
+                self.assertEqual(w,[0,2,4,6])
+
+            def test_f5_6(self):
+                w=przyklad.f5(17,2)
+                self.assertEqual(w,[0,2,4,6,8,10,12,14,16])
+
+            def test_f5_7(self):
+                w=przyklad.f5(17,5)
+                self.assertEqual(w,[0,5,10,15])
+
+            def test_f6_1(self):
+                w=przyklad.f6(1)
+                self.assertEqual(w,"*")
+
+            def test_f6_2(self):
+                w=przyklad.f6(7)
+                self.assertEqual(w,"*******")
+
+            def test_f7_1(self):
+                w=przyklad.f7("ala")
+                self.assertEqual(w,"slowo")
+
+            def test_f7_2(self):
+                w=przyklad.f7(1)
+                self.assertEqual(w,"cyfra")
+
+            def test_f7_2(self):
+                w=przyklad.f7(11111)
+                self.assertEqual(w,"liczba")
+
+            def test_f7_3(self):
+                w=przyklad.f7(-11111)
+                self.assertEqual(w,"liczba ze znakiem")
+
+
+
+
+
+
+
+
 
 
 
